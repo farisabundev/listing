d810ffe28562bdeb2df0619bda95ba8b3afd7561
Faris Imaduddin <faris.imaduddin@koala.id>
2020-09-17 12:00:02 +0700

    Add react-loadable, 404 default page, etc

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+const Home = () => {
+    return(
+        <div>
+            <Row>
+                <Col>Koala Listing</Col>
+                <Col>Koala Listings</Col>
+            </Row>
+        </div>
+    )
+}
+
+export default Home;
